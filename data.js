d1e453bd660e2ccaa96774bbec0f521d373e8c2b
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-01-31 14:40:11 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,471 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-21--overview-1",
+      "name": "SF-21- Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2596453077470926,
+        "pitch": 0.6725773398723991,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6113014868889106,
+          "pitch": 0.48173361219970623,
+          "rotation": 0,
+          "target": "3-sf-21--old-transformer"
+        },
+        {
+          "yaw": 0.2483575530608615,
+          "pitch": 0.1680863177269245,
+          "rotation": 0,
+          "target": "1-sf-21--overview-2"
+        },
+        {
+          "yaw": 0.1778668639996468,
+          "pitch": 0.2925776157145936,
+          "rotation": 0,
+          "target": "5-sf-21--wellpad-2"
+        },
+        {
+          "yaw": -0.0265102751592714,
+          "pitch": 0.40612282856489657,
+          "rotation": 0,
+          "target": "6-sf-21-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -0.08302230594686577,
+          "pitch": 0.6788470856528743,
+          "rotation": 0,
+          "target": "2-sf-21--security-building"
+        },
+        {
+          "yaw": -0.19401911873862687,
+          "pitch": 0.3093973942938355,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-21--overview-2",
+      "name": "SF-21- Overview 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.294208168939015,
+        "pitch": 0.07524531341084284,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1088678603515838,
+          "pitch": 0.02563366618051255,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": 0.07713221504644707,
+          "pitch": 0.29214807205071835,
+          "rotation": 0,
+          "target": "6-sf-21-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.2998488156372954,
+          "pitch": 0.184971222369823,
+          "rotation": 0,
+          "target": "3-sf-21--old-transformer"
+        },
+        {
+          "yaw": -0.06130939997801654,
+          "pitch": 0.4906891049207598,
+          "rotation": 0,
+          "target": "5-sf-21--wellpad-2"
+        },
+        {
+          "yaw": 0.5375386752693814,
+          "pitch": 0.3225555787648702,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-21--security-building",
+      "name": "SF-21- Security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.2028551548381863,
+        "pitch": 0.11443761537386621,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0025695670775043,
+          "pitch": 0.02065995835384271,
+          "rotation": 0,
+          "target": "3-sf-21--old-transformer"
+        },
+        {
+          "yaw": 2.3591448695232113,
+          "pitch": 0.06553517941982534,
+          "rotation": 0,
+          "target": "6-sf-21-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 2.1667778813833802,
+          "pitch": -0.006606020643616262,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        },
+        {
+          "yaw": -0.768716393751987,
+          "pitch": -0.4443693153268722,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": 2.861692839044945,
+          "pitch": -0.28335849256854395,
+          "rotation": 0,
+          "target": "1-sf-21--overview-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-21--old-transformer",
+      "name": "SF-21- Old transformer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.8582267073627747,
+        "pitch": 0.3775017052668801,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.046779599399052,
+          "pitch": -0.34961479512701743,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": 2.8357190806036137,
+          "pitch": 0.14710840179167484,
+          "rotation": 0,
+          "target": "2-sf-21--security-building"
+        },
+        {
+          "yaw": 0.9420572254176083,
+          "pitch": -0.03775428692891758,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        },
+        {
+          "yaw": 1.6528414448357553,
+          "pitch": 0.0006362610251535727,
+          "rotation": 0,
+          "target": "6-sf-21-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 1.5617040781047118,
+          "pitch": -0.49566315116691406,
+          "rotation": 0,
+          "target": "1-sf-21--overview-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-21--wellpad-1",
+      "name": "SF-21- WellPad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.9631033534300562,
+        "pitch": 0.29211852583440745,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.059526783178079,
+          "pitch": 0.15058390445055103,
+          "rotation": 0,
+          "target": "3-sf-21--old-transformer"
+        },
+        {
+          "yaw": 1.2189492951803693,
+          "pitch": 0.006886104113615232,
+          "rotation": 0,
+          "target": "5-sf-21--wellpad-2"
+        },
+        {
+          "yaw": 2.7088470870593238,
+          "pitch": 0.09855354817662132,
+          "rotation": 0,
+          "target": "2-sf-21--security-building"
+        },
+        {
+          "yaw": 2.814906089645641,
+          "pitch": -0.13324393723496897,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": 0.8328997728937662,
+          "pitch": -0.28869517972011316,
+          "rotation": 0,
+          "target": "1-sf-21--overview-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-21--wellpad-2",
+      "name": "SF-21- WellPad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.700077765105421,
+        "pitch": 0.013847715415860051,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.022369006155637,
+          "pitch": 0.04715037559591195,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        },
+        {
+          "yaw": 2.408308227683909,
+          "pitch": 0.12368482877761622,
+          "rotation": 0,
+          "target": "6-sf-21-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 2.235115097972095,
+          "pitch": -0.22486359322895666,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": 1.9651196047897797,
+          "pitch": 0.01751626200827161,
+          "rotation": 0,
+          "target": "2-sf-21--security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-21-sucker-rod-pumping-system",
+      "name": "SF-21-Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.3521773724302584,
+        "pitch": 0.08797913568035653,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26879116160461436,
+          "pitch": -0.05399826132253516,
+          "rotation": 0,
+          "target": "2-sf-21--security-building"
+        },
+        {
+          "yaw": -3.1126317098640364,
+          "pitch": 0.09711502473445854,
+          "rotation": 0,
+          "target": "5-sf-21--wellpad-2"
+        },
+        {
+          "yaw": 0.5232434806373938,
+          "pitch": -0.01266537456759842,
+          "rotation": 0,
+          "target": "3-sf-21--old-transformer"
+        },
+        {
+          "yaw": 2.2090827152589503,
+          "pitch": 0.04854252976884332,
+          "rotation": 0,
+          "target": "4-sf-21--wellpad-1"
+        },
+        {
+          "yaw": -0.1675125791445531,
+          "pitch": -0.33329244275304504,
+          "rotation": 0,
+          "target": "0-sf-21--overview-1"
+        },
+        {
+          "yaw": -3.052312181638884,
+          "pitch": -0.28012864400440307,
+          "rotation": 0,
+          "target": "1-sf-21--overview-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-21",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
